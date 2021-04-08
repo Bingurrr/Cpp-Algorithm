57f207f557321dae13104d91fd68dbcc619d9fa1
박병규 <39543006+kookminswpark@users.noreply.github.com>
2021-04-08 21:47:59 +0900

    Create 농장.cpp
    C++프로그래밍 과제

---
+++ 농장.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a, b, n, w, x, y;
+    cin >> a >> b >> n >> w;
+    if (a == b) {
+        cout << -1;
+        return 0;
+    }
+    else if ((w - n * b) * (a - b) <= 0) {
+        cout << -1;
+        x = -1;
+        y = -1;
+        return 0;
+    }
+
+    else {
+        x = (w - n * b) / (a - b);
+        if (x <= 0) {
+            cout << -1;
+            return 0;
+        }
+    }
+
+    y = n - x;
+    cout << x << ' ' << y << endl;
+    return 0;
+}
