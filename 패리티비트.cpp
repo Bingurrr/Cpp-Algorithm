ba36cdd3f65c16ae2557d8772419443ea6f56418
박병규 <39543006+Bingurrr@users.noreply.github.com>
2021-04-20 21:52:28 +0900

    Update 패리티비트.cpp

--- 패리티비트.cpp
+++ 패리티비트.cpp
@@ -1,26 +1,17 @@
 #include <iostream>
-#include<cmath>;
-
 using namespace std;
 
-int main() {
+int main(void) {
+	unsigned int n, v;
+	bool parity = false;
 
-	int num = 1;
-	long long answer = num;
-	int count = 0;
-
-	while (num > 0){
-		if ((num%2) == 1) {
-			count++;
-		}
-		num = num / 2;
-
+	cin >> n;
+	v = n & 0x7FFFFFFF;
+	for (int i = 0; i < 31; i++, v >>= 1) {
+		if (v % 2) parity = !parity;
 	}
-	if ((count%2) == 1) {
-		answer += pow(2,31);
-	}
-	cout << answer;
-
+	if (parity) { n += 0x80000000; }
+	cout << n << endl;
 
 	return 0;
 }
